084bd45d74e62a93aa4c517be2303532c8414793
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-22 11:33:27 +0200

    after creating additional Engine+ Wheel classes we create logt to create private static class to provide log message with the name of the class

---
+++ app/src/main/java/com/example/myapplication_dagger/Car.java
@@ -0,0 +1,5 @@
+package com.example.myapplication_dagger;
+
+public class Car {
+    private static final String TAG = "Car";
+}
---
+++ app/src/main/java/com/example/myapplication_dagger/Engine.java
@@ -0,0 +1,4 @@
+package com.example.myapplication_dagger;
+
+public class Engine {
+}
