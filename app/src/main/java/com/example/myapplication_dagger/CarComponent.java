11b6ab189f04498c9874007e4f5f053458902c43
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-22 11:52:49 +0200

    creating CarComponent interface w/@Component annotation

---
+++ app/src/main/java/com/example/myapplication_dagger/CarComponent.java
@@ -0,0 +1,9 @@
+package com.example.myapplication_dagger;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+
+    Car getCar();
+}
--- app/src/main/java/com/example/myapplication_dagger/MainActivity.java
+++ app/src/main/java/com/example/myapplication_dagger/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        car.drive();
     }
 }
