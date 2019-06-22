ef0d5f4c64c9a01644fd2aa54ae0f5f06e54efb5
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-22 11:45:24 +0200

    log was generated as a confirmation carparts were implemented successfully

--- app/src/main/java/com/example/myapplication_dagger/Car.java
+++ app/src/main/java/com/example/myapplication_dagger/Car.java
@@ -1,15 +1,20 @@
 package com.example.myapplication_dagger;
 
+import android.util.Log;
+
 public class Car {
     private static final String TAG = "Car";
 
     private Engine engine;
     private Wheels wheels;
 
-    // now we allow Android studio generate construckors for above private carparts
+    // now we allow Android studio generate public constructors for above private carparts
 
     public Car(Engine engine, Wheels wheels) {
         this.engine = engine;
         this.wheels = wheels;
+    }
+    public void drive(){
+        Log.d(TAG, "driving...");
     }
 }
