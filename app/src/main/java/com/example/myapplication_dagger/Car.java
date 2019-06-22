863b64bd5fbcbf9270afec1a7450356bd1da0a68
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-22 11:38:19 +0200

    instantiating private carparts in a Car class and generating their constructors

--- app/src/main/java/com/example/myapplication_dagger/Car.java
+++ app/src/main/java/com/example/myapplication_dagger/Car.java
@@ -2,4 +2,14 @@ package com.example.myapplication_dagger;
 
 public class Car {
     private static final String TAG = "Car";
+
+    private Engine engine;
+    private Wheels wheels;
+
+    // now we allow Android studio generate construckors for above private carparts
+
+    public Car(Engine engine, Wheels wheels) {
+        this.engine = engine;
+        this.wheels = wheels;
+    }
 }
