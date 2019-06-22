e503704a0035390d4b3805b82b96d555c109ca21
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-22 12:41:29 +0200

    @Inject has to be annotated for both Engine and Wheel class constructor

--- app/src/main/java/com/example/myapplication_dagger/Engine.java
+++ app/src/main/java/com/example/myapplication_dagger/Engine.java
@@ -1,4 +1,13 @@
 package com.example.myapplication_dagger;
 
+import javax.inject.Inject;
+
 public class Engine {
+
+    //creating class constructor and annotating @Inject
+
+    @Inject
+    public Engine(){
+
+    }
 }
--- app/src/main/java/com/example/myapplication_dagger/Wheels.java
+++ app/src/main/java/com/example/myapplication_dagger/Wheels.java
@@ -1,4 +1,13 @@
 package com.example.myapplication_dagger;
 
+import javax.inject.Inject;
+
 public class Wheels {
+
+    //creating class constructor and annotating @Inject
+
+    @Inject
+    public Wheels(){
+
+    }
 }
